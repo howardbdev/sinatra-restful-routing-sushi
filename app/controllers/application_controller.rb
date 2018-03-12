7612d5b848990e1e1a6d466f7f25afecb63d9060
howardbdev <howarddevennish@gmail.com>
2018-03-12 17:55:22 -0400

    add solution code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,45 @@ class ApplicationController < Sinatra::Base
   set :views, "app/views"
 
   get "/" do
+    redirect "/sushis"
+  end
+
+  # get all sushis
+  get "/sushis" do
+    @sushis = Sushi.all
     erb :index
   end
 
-  get "/i-wanna-redirect" do
+  # render form to create a sushi
+  get "/sushis/new" do
+    erb :new
+  end
+
+  # get one sushi
+  get "/sushis/:id" do
+    @sushi = Sushi.find(params[:id])
+    puts @sushi
+    erb :show
+  end
+
+  # edit one sushi
+  get "/sushis/:id/edit" do
+    @sushi = Sushi.find(params[:id])
+    erb :edit
+  end
+
+  # create a sushi
+  post "/sushis" do
+    sushi = Sushi.create(name: params[:name], description: params[:description])
+    redirect "/sushis"
+  end
+
+  # delete a sushi
+  delete "/sushis/:id" do
+    binding.pry
+    @sushi = Sushi.find(params[:id])
+    @sushi.delete
     redirect "/"
   end
+
 end
